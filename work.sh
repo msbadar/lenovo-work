d3efba1c611540943295eaa0dfe97c53a5b8ed06
BADAR <msbadar@gmail.com>
2026-09-14 00:24:02 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -6,19 +6,35 @@ log() {
 }
 
 IMAGE_NAME="sandbox-rn-claude-claude"
-cd ~/workspace/repo-maker || exit 1
-
 log "Checking if container is running..."
-
 if podman ps --format "{{.Image}}" | grep -q "${IMAGE_NAME}"; then
     log "Container '${IMAGE_NAME}' is already running. Exiting."
     exit 0
 fi
 
+# Working on udaan
+cd ~/workspace/udaan || exit 1
+
+# Prompt
 read -r -d '' PROMPT << 'EOM' || true
-work on pending items listed in plan.md. commit changes once finished
+TODO: 
+- create github actions to deploy to cloud run 
+- work on pending items listed in plan.md.
+- commit changes once finished
 EOM
 
+# invoking agent
 log "Invoking agent with prompt..."
 ccr "$PROMPT"
 log "Agent run finished."
+ccr "commit changes"
+
+# Pushing changes
+git push origin development
+
+
+# pushing logs
+cd ~/workspace/work || exit 1
+git add .
+gi commit -m "logs +%F %T "
+git push origin development
