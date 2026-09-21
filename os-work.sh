9a95b9cf74d19d018eb1ecab330d20e4584e35c7
BADAR <msbadar@gmail.com>
2026-09-21 23:43:17 +0530

    Update os-work.sh

--- os-work.sh
+++ os-work.sh
@@ -46,7 +46,7 @@ if lsof -Pi :"$PORT" -sTCP:LISTEN -t >/dev/null 2>&1; then
 else
   echo "Port $PORT is free. Starting terminal..."
   cd ~/workspace/terminal
-  ./dist/webterm --port "$PORT" &
+  WEBTERM_STATIC_DIR=dist/web ./dist/webterm --port "$PORT" &
   TERM_PID=$!
   wait_for_port "$PORT" "$TERM_PID" "webterm"
 fi
