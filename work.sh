6e26b7827172b29d13e0850bcd99cd14ba1860b7
BADAR <msbadar@gmail.com>
2026-09-22 23:46:41 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -5,3 +5,13 @@ set -eo pipefail
 # Terminal
 cd ~/workspace/terminal
 WEBTERM_STATIC_DIR=dist/web nohup ./dist/webterm --port 4000 --base-path /terminal > webterm.log 2>&1 &
+
+
+# Tunnel
+if podman ps --format '{{.Image}}' 2>/dev/null | grep -qi "cloudflared"; then
+  echo "Cloudflared container is already running. Skipping tunnel."
+else
+  echo "Starting Cloudflare tunnel..."
+  cd ~/workspace/tunnel
+  bash ./tunnel.sh
+fi
