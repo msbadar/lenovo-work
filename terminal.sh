1892eaf47fcf781d9d798a51e6cc9d25f536f85a
BADAR <msbadar@gmail.com>
2026-09-22 23:20:58 +0530

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 
-kill $(lsof -t -i :4000)
-kill $(lsof -t -i :4001)
-kill $(lsof -t -i :8080)
-kill $(lsof -t -i :8000)
-podman kill --all
-
 PORT=4000
+
+kill $(lsof -t -i :$PORT)
+
 if lsof -Pi :"$PORT" -sTCP:LISTEN -t >/dev/null 2>&1; then
   echo "Port $PORT is already in use. Skipping terminal."
 else
