54f043c7a89ae0341ba749678ceb9cdef850c2a7
BADAR <msbadar@gmail.com>
2026-09-22 23:17:29 +0530

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,1 +1,7 @@
-
+if podman ps --format '{{.Image}}' 2>/dev/null | grep -qi "cloudflared"; then
+  echo "Cloudflared container is already running. Skipping tunnel."
+else
+  echo "Starting Cloudflare tunnel..."
+  cd ~/workspace/tunnel
+  bash ./tunnel.sh
+fi
