77295d83c70df74c7dc47a8a40bc07ad5070e76b
BADAR <msbadar@gmail.com>
2026-09-23 00:07:52 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
-# Terminal
-cd ~/workspace/terminal
-WEBTERM_STATIC_DIR=dist/web nohup ./dist/webterm --port 4000 --base-path /terminal > webterm.log 2>&1 &
+# # Terminal
+# cd ~/workspace/terminal
+# WEBTERM_STATIC_DIR=dist/web nohup ./dist/webterm --port 4000 --base-path /terminal > webterm.log 2>&1 &
 
 # Tunnel
 if podman ps --filter "name=cloudflared" --filter "status=running" -q 2>/dev/null | grep -q .; then
