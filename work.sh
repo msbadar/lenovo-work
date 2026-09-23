7fe087bd56d9b5d80af039fd73945bdb49379729
BADAR <msbadar@gmail.com>
2026-09-23 12:03:08 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -2,9 +2,9 @@
 set -eo pipefail
 
 
-chmod +x /home/badar/workspace/work/run.sh
+# chmod +x /home/badar/workspace/work/run.sh
 
-crontab -l 2>/dev/null | grep -Fq "/home/badar/workspace/work/run.sh" || (crontab -l 2>/dev/null; echo "* * * * * /home/badar/workspace/work/run.sh") | crontab -
+# crontab -l 2>/dev/null | grep -Fq "/home/badar/workspace/work/run.sh" || (crontab -l 2>/dev/null; echo "* * * * * /home/badar/workspace/work/run.sh") | crontab -
 
 
 # # Cleanup
@@ -49,22 +49,22 @@ crontab -l 2>/dev/null | grep -Fq "/home/badar/workspace/work/run.sh" || (cronta
 # fi
 
 # # LOGS
-# podman ps > containers.txt
-# {
-#   printf "%-10s %-10s %-15s\n" "PORT" "PID" "COMMAND"
-#   for port in 4000 4001 4002 8080 8000; do
-#     for pid in $(lsof -t -i :"$port" 2>/dev/null || true); do
-#       cmd=$(ps -p "$pid" -o comm= 2>/dev/null || echo "unknown")
-#       printf "%-10s %-10s %-15s\n" "$port" "$pid" "$cmd"
-#     done
-#   done
-# } > ports.txt
+podman ps > run.txt
+{
+  printf "%-10s %-10s %-15s\n" "PORT" "PID" "COMMAND"
+  for port in 4000 4001 4002 8080 8000; do
+    for pid in $(lsof -t -i :"$port" 2>/dev/null || true); do
+      cmd=$(ps -p "$pid" -o comm= 2>/dev/null || echo "unknown")
+      printf "%-10s %-10s %-15s\n" "$port" "$pid" "$cmd"
+    done
+  done
+} > run.txt
 
 # # Commit and push only if changes exist
-# git add .
-# if ! git diff-index --quiet HEAD --; then
-#   git commit -m "logs update $(date +'%Y-%m-%d %H:%M')"
-#   git push origin development
-# else
-#   echo "No log changes to commit."
-# fi
+git add .
+if ! git diff-index --quiet HEAD --; then
+  git commit -m "logs update $(date +'%Y-%m-%d %H:%M')"
+  git push origin development
+else
+  echo "No log changes to commit."
+fi
