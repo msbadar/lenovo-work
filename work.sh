ba2482fbe97037cad88a6fda78f1a0a54be3f064
BADAR <msbadar@gmail.com>
2026-09-23 14:21:53 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -48,17 +48,18 @@ echo "run $(date +'%Y-%m-%d %H:%M')" > run.txt
 #   )
 # fi
 
-# # LOGS
-# podman ps >> run.txt
-# {
-#   printf "%-10s %-10s %-15s\n" "PORT" "PID" "COMMAND"
-#   for port in 4000 4001 4002 8080 8000; do
-#     for pid in $(lsof -t -i :"$port" 2>/dev/null || true); do
-#       cmd=$(ps -p "$pid" -o comm= 2>/dev/null || echo "unknown")
-#       printf "%-10s %-10s %-15s\n" "$port" "$pid" "$cmd"
-#     done
-#   done
-# } >> run.txt
+
+# LOGS
+podman ps >> run.txt
+{
+  printf "%-10s %-10s %-15s\n" "PORT" "PID" "COMMAND"
+  for port in 4000 4001 4002 8080 8000; do
+    for pid in $(lsof -t -i :"$port" 2>/dev/null || true); do
+      cmd=$(ps -p "$pid" -o comm= 2>/dev/null || echo "unknown")
+      printf "%-10s %-10s %-15s\n" "$port" "$pid" "$cmd"
+    done
+  done
+} >> run.txt
 
 # # Commit and push only if changes exist
 git add .
