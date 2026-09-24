310606e66bc3742260560e1cb1ebc5fee04dbaaf
BADAR <msbadar@gmail.com>
2026-09-24 13:39:50 +0530

    Update work.sh

--- work.sh
+++ work.sh
@@ -25,13 +25,13 @@ echo "run $(date +'%Y-%m-%d %H:%M')" > run.txt
 
 
 # Terminal
-PORT=4002
-if ! ss -tuln | grep -q ":${PORT}\b"; then
-  (
-    cd ~/workspace/terminal || exit 1
-    WEBTERM_STATIC_DIR=dist/web ./dist/webterm --port "$PORT"
-  )
-fi
+# PORT=4002
+# if ! ss -tuln | grep -q ":${PORT}\b"; then
+#   (
+#     cd ~/workspace/terminal || exit 1
+#     WEBTERM_STATIC_DIR=dist/web ./dist/webterm --port "$PORT"
+#   )
+# fi
 
 # # # Nova OS
 # (
